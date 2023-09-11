7ab4751238520c2a91a0dee52c1fefd18a08e0ed
SuperKgePluss <ma.puttichawat@outlook.com>
2023-09-11 21:48:23 +0700

    Configuring Stock and Price
    declare stock and price value then show them on the Row UI elements

--- Assets/Scripts/Shops/Shop.cs
+++ Assets/Scripts/Shops/Shop.cs
@@ -8,12 +8,23 @@ namespace RPG.Shops {
     public class Shop : MonoBehaviour, IRaycastable {
         [SerializeField] string shopName;
 
+        //Stock Config
+        [SerializeField] StockItemConfig[] stockConfig;
+
+        [Serializable]
+        class StockItemConfig {
+            public InventoryItem item;
+            public int initialStock;
+            [Range(0, 100)] public float buyingDiscountPercentage;
+        }
+
         public event Action onChange;
 
         public IEnumerable<ShopItem> GetFilteredItems() {
-            yield return new ShopItem(InventoryItem.GetFromID("e75a0c32-d41c-4651-8496-92cb958a8f1e"), 10, 10.0f, 0);
-            yield return new ShopItem(InventoryItem.GetFromID("28c6f2e6-46e9-4879-a14f-d6998c781cb7"), 10, 1000.99f, 0);
-            yield return new ShopItem(InventoryItem.GetFromID("dbc1e40e-d3bd-4e26-a62b-6cff0e46c415"), 10, 10.0f, 0);
+            foreach (StockItemConfig config in stockConfig) {
+                float price = config.item.GetPrice() * (1 - config.buyingDiscountPercentage / 100f);
+                yield return new ShopItem(config.item, config.initialStock, price, 0);
+            }
         }
         public void SelectFilter(ItemCategory category) { }
         public ItemCategory GetFilter() { return ItemCategory.None; }
