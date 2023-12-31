f1927973d3a1958d4bead9b01cdb19a0e525634e
SuperKgePluss <ma.puttichawat@outlook.com>
2023-12-31 19:22:17 +0700

    Triggering the action bar
    Create a new ability system for a player to use and purchase from the shop.

---
+++ Assets/Scripts/Abilities/Ability.cs
@@ -0,0 +1,9 @@
+using GameDevTV.Inventories;
+using UnityEngine;
+
+namespace RPG.Abilities {
+    [CreateAssetMenu(fileName = "My Ability", menuName = "Abilities/Ability", order = 0)]
+    public class Ability : ActionItem {
+
+    }
+}
--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,20 +1,18 @@
-using RPG.Combat;
-using RPG.Movement;
-using UnityEngine;
+using GameDevTV.Inventories;
 using RPG.Attributes;
+using RPG.Movement;
 using System;
-using UnityEngine.EventSystems;
+using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.EventSystems;
 
-namespace RPG.Control
-{
-    public class PlayerController : MonoBehaviour
-    {
+namespace RPG.Control {
+    public class PlayerController : MonoBehaviour {
         Health health;
+        ActionStore actionStore;
 
         [System.Serializable]
-        struct CursorMapping
-        {
+        struct CursorMapping {
             public CursorType type;
             public Texture2D texture;
             public Vector2 hotspot;
@@ -23,21 +21,23 @@ namespace RPG.Control
         [SerializeField] CursorMapping[] cursorMappings = null;
         [SerializeField] float maxNavMeshProjectionDistance = 1f;
         [SerializeField] float raycastRadius = 1f;
+        [SerializeField] int numberOfAbilities = 6;
 
         bool isDraggingUI = false;
 
         private void Awake() {
             health = GetComponent<Health>();
+            actionStore = GetComponent<ActionStore>();
         }
 
-        private void Update()
-        {
+        private void Update() {
             if (InteractWithUI()) return;
-            if (health.IsDead()) 
-            {
+            if (health.IsDead()) {
                 SetCursor(CursorType.None);
                 return;
             }
+
+            UseAbilities();
 
             if (InteractWithComponent()) return;
             if (InteractWithMovement()) return;
@@ -45,38 +45,37 @@ namespace RPG.Control
             SetCursor(CursorType.None);
         }
 
-        private bool InteractWithUI()
-        {
-            if (Input.GetMouseButtonUp(0))
-            {
+        private bool InteractWithUI() {
+            if (Input.GetMouseButtonUp(0)) {
                 isDraggingUI = false;
             }
-            if (EventSystem.current.IsPointerOverGameObject())
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
+            if (EventSystem.current.IsPointerOverGameObject()) {
+                if (Input.GetMouseButtonDown(0)) {
                     isDraggingUI = true;
                 }
                 SetCursor(CursorType.UI);
                 return true;
             }
-            if (isDraggingUI)
-            {
+            if (isDraggingUI) {
                 return true;
             }
             return false;
         }
 
-        private bool InteractWithComponent()
-        {
+        private void UseAbilities() {
+            for (int i = 0; i < numberOfAbilities; i++) {
+                if (Input.GetKeyDown(KeyCode.Alpha1 + i)) {
+                    actionStore.Use(i, gameObject);
+                }
+            }
+        }
+
+        private bool InteractWithComponent() {
             RaycastHit[] hits = RaycastAllSorted();
-            foreach (RaycastHit hit in hits)
-            {
+            foreach (RaycastHit hit in hits) {
                 IRaycastable[] raycastables = hit.transform.GetComponents<IRaycastable>();
-                foreach (IRaycastable raycastable in raycastables)
-                {
-                    if (raycastable.HandleRaycast(this))
-                    {
+                foreach (IRaycastable raycastable in raycastables) {
+                    if (raycastable.HandleRaycast(this)) {
                         SetCursor(raycastable.GetCursorType());
                         return true;
                     }
@@ -85,28 +84,23 @@ namespace RPG.Control
             return false;
         }
 
-        RaycastHit[] RaycastAllSorted()
-        {
+        RaycastHit[] RaycastAllSorted() {
             RaycastHit[] hits = Physics.SphereCastAll(GetMouseRay(), raycastRadius);
             float[] distances = new float[hits.Length];
-            for (int i = 0; i < hits.Length; i++)
-            {
+            for (int i = 0; i < hits.Length; i++) {
                 distances[i] = hits[i].distance;
             }
             Array.Sort(distances, hits);
             return hits;
         }
 
-        private bool InteractWithMovement()
-        {
+        private bool InteractWithMovement() {
             Vector3 target;
             bool hasHit = RaycastNavMesh(out target);
-            if (hasHit)
-            {
+            if (hasHit) {
                 if (!GetComponent<Mover>().CanMoveTo(target)) return false;
 
-                if (Input.GetMouseButton(0))
-                {
+                if (Input.GetMouseButton(0)) {
                     GetComponent<Mover>().StartMoveAction(target, 1f);
                 }
                 SetCursor(CursorType.Movement);
@@ -115,8 +109,7 @@ namespace RPG.Control
             return false;
         }
 
-        private bool RaycastNavMesh(out Vector3 target)
-        {
+        private bool RaycastNavMesh(out Vector3 target) {
             target = new Vector3();
 
             RaycastHit hit;
@@ -133,26 +126,21 @@ namespace RPG.Control
             return true;
         }
 
-        private void SetCursor(CursorType type)
-        {
+        private void SetCursor(CursorType type) {
             CursorMapping mapping = GetCursorMapping(type);
             Cursor.SetCursor(mapping.texture, mapping.hotspot, CursorMode.Auto);
         }
 
-        private CursorMapping GetCursorMapping(CursorType type)
-        {
-            foreach (CursorMapping mapping in cursorMappings)
-            {
-                if (mapping.type == type)
-                {
+        private CursorMapping GetCursorMapping(CursorType type) {
+            foreach (CursorMapping mapping in cursorMappings) {
+                if (mapping.type == type) {
                     return mapping;
                 }
             }
             return cursorMappings[0];
         }
 
-        private static Ray GetMouseRay()
-        {
+        private static Ray GetMouseRay() {
             return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
